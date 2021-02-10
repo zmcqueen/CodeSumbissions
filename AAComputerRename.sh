1b8ec2aa7bb064d9016f17e3978fa1a93df5e267
zmcqueen <49767228+zmcqueen@users.noreply.github.com>
2021-02-10 10:42:06 -0800

    final edit before submission

--- AAComputerRename.sh
+++ AAComputerRename.sh
@@ -5,9 +5,9 @@
 
 # Set variables here:
 maxStringLength=63
-apiURL="https://jss.appannie.org:10023/JSSResource/computers/macaddress/"
-apiUser="api-read-sites"#This should be replaced with $4 in Jamf
-apiPass="rzKnVB3bnV@UfMCFFj7iu8X*@PYU"#This should be replaced with $5 in Jamf
+apiURL="https://jss.appannie.org:10023/JSSResource/computers/macaddress/" # could be replaced with $6 in Jamf if we want to keep our URL private also
+apiUser=$4 
+apiPass=$5
 MacAdd=$( /usr/sbin/networksetup -getmacaddress en0 | /usr/bin/awk '{ print $3 }' | /usr/bin/sed 's/:/./g' )
 
 setNames() {
@@ -48,6 +48,7 @@ hostName=$(echo "$preProcessedComputerName" | awk '{print toupper($0)}')
 
 # ~~~~~~~~~~~~~
 
+# Fetch site name from Jamf API
 siteName=$( /usr/bin/curl -sku $apiUser:$apiPass $apiURL$MacAdd/subset/general  -X GET -H "Accept: application/xml"  | /usr/bin/xpath /computer/general/site/name |/usr/bin/sed 's/<name>//;s/<\/name>//' )
 if [[ "$siteName" != "" ]]; then
     echo "$siteName"
@@ -55,6 +56,7 @@ else
         echo "Not Available"
 fi
 
+# Get shortened site name
 case $siteName in
 "Beijing")
     siteAbbreviation="BJ"
@@ -108,7 +110,7 @@ esac
 
 echo "Shortened to: $siteAbbreviation"
 
-# figure out the user
+# figure out the local user
 user=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 echo "User: " + "$user"
 
@@ -124,10 +126,10 @@ echo "$firstInitial"
 lastName="$(echo "$name" | cut -d \  -f 2)"
 echo "Last: " + "$lastName"
 
-# DEBUG BLOCK
-lastName="mcqueen's big long name with all kinds of garbage in here234523452345! It %@(*@%) apparently needs to be even longer??????"
+# DEBUG BLOCK to check text trimming and character cleaning
+lastName="user's big long name with all kinds of garbage in here234523452345! It %@(*@%) apparently needs to be even longer??????"
 
-# add first and last together
+# add first initial and last name together
 preProcessedComputerName=$(echo "AA$siteAbbreviation$firstInitial$lastName")
 echo "computer name: " + "$preProcessedComputerName"
 
@@ -137,6 +139,7 @@ preProcessedComputerName=$(echo "$preProcessedComputerName" | tr -cd "[:alnum:]"
 # clean up preProcessedComputerName to have all upper case
 hostName=$(echo "$preProcessedComputerName" | awk '{print toupper($0)}')
 
+# Check the length of the name after cleaning and trim if necessary
 hostNameLength=${#hostName}
 echo "$hostNameLength"
 if [ "$hostNameLength" -gt 62 ]
@@ -146,3 +149,7 @@ then
     echo "Reduced to ${#hostName} characters"
 fi
 echo "$hostName"
+
+setNames
+reportNamesToJamf
+exit 0
